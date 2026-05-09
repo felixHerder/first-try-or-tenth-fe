a8b56482acffb5268f504767a665d43d07692a59
felixHerder <felixbaciu@gmail.com>
2026-05-09 16:56:30 +0300

    add lazy loading loader

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouteConfigLoadEnd, RouteConfigLoadStart, Router, RouterOutlet } from '@angular/router';
+import { LoaderService } from '@core/services/loader.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  private loaderService = inject(LoaderService);
+  private router = inject(Router);
+  loading = this.loaderService.loading;
+
+  constructor() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof RouteConfigLoadStart) {
+        this.loading.set(true);
+      } else if (event instanceof RouteConfigLoadEnd) {
+        this.loading.set(false);
+      }
+    });
+  }
+}
